bc14001e7b0475d85f969d7b6003f7e61e337575
Sergio Hilgert <sergio.hilgert@redealumni.com>
2020-08-26 23:46:47 -0300

    update text

--- src/pages/landing-page/landing-page.ts
+++ src/pages/landing-page/landing-page.ts
@@ -2,6 +2,7 @@ import Component from 'decorators/component';
 import { DepoimentoSection } from './sections/depoimentos-section/depoimentos-section';
 import { AssinaturasSection } from './sections/assinaturas-section/assinaturas-section';
 import { HeaderSection } from './sections/header-section/header-section';
+import { DescriptionSection } from './sections/description-section/description-section';
 
 @Component({
   selector: 'landing-page',
@@ -12,6 +13,7 @@ export class LandingPage extends HTMLElement {
   constructor() {
     super();
 
+    new DescriptionSection();
     new HeaderSection();
     new DepoimentoSection();
     new AssinaturasSection();
